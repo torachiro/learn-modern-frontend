50176123a666635623ac9f431ad8bc01d75797de
goi <ptn-ruiko.goi@zozo.com>
2023-03-20 18:21:55 +0900

    npx graphql-codegen init

---
+++ codegen.ts
@@ -0,0 +1,16 @@
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "https://valid-stingray-46.hasura.app/v1/graphql",
+  documents: "queries/**/*.ts",
+  generates: {
+    "types/generated/graphql.tsx": {
+      preset: "client",
+      plugins: []
+    }
+  }
+};
+
+export default config;

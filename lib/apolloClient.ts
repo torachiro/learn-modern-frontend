c8d90b8230775cab2eb9be09a48683274f99902b
goi <ptn-ruiko.goi@zozo.com>
2023-03-28 04:50:56 +0900

    apolloclientの設定変更

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -12,7 +12,10 @@ const createApolloClient = () => {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
     link: new HttpLink({
-      uri: 'https://valid-stingray-46.hasura.app/v1/graphql',
+      uri: process.env.NEXT_PUBLIC_HASURA_URL,
+      headers: {
+        'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_KEY,
+      },
     }),
     cache: new InMemoryCache(),
   })

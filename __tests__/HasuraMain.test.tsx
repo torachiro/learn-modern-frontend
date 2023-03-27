20a38264c4e041d6a9c17cc7f0a5883391a0f505
goi <ptn-ruiko.goi@zozo.com>
2023-03-28 03:51:44 +0900

    hasura mainページのテストコート追加

---
+++ __tests__/HasuraMain.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
+import { getPage, initTestHelpers } from 'next-page-tester'
+import { setupServer } from 'msw/node'
+import { handlers } from '../mock/handlers'
+
+initTestHelpers()
+
+const server = setupServer(...handlers)
+beforeAll(() => server.listen())
+afterEach(() => {
+  server.resetHandlers()
+  cleanup()
+})
+afterAll(() => server.close())
+
+describe('Hasura Fetch Test Cases', () => {
+  it('Should render the list of users by useQuery', async () => {
+    const { page } = await getPage({
+      route: '/hasura-main',
+    })
+    render(page)
+    expect(await screen.findByText('Hasura Main Page')).toBeInTheDocument()
+    expect(await screen.findByText('Test user A')).toBeInTheDocument()
+    expect(screen.getByText('Test user B')).toBeInTheDocument()
+    expect(screen.getByText('Test user C')).toBeInTheDocument()
+  })
+})

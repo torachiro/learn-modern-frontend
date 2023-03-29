732d842f5163e00db0ee2ccb22eb925c63549ea4
goi <ptn-ruiko.goi@zozo.com>
2023-03-29 09:21:52 +0900

    codegenの設定変更

--- codegen.ts
+++ codegen.ts
@@ -2,8 +2,18 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'https://valid-stingray-46.hasura.app/v1/graphql',
+  schema: [
+    {
+      'https://valid-stingray-46.hasura.app/v1/graphql': {
+        headers: {
+          'x-hasura-admin-secret':
+            process.env.NEXT_PUBLIC_HASURA_KEY || 'hasuratest1123',
+        },
+      },
+    },
+  ],
   documents: 'queries/**/*.ts',
+  ignoreNoDocuments: true,
   generates: {
     'types/generated/': {
       preset: 'client',
